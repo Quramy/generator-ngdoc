11eb87fcffedb8e62a517b80094de01b7239c9f2
Yosuke Kurami <yosuke.kurami@gmail.com>
2014-12-05 17:16:35 +0900

    Modify tests

--- test/test-app.js
+++ test/test-app.js
@@ -11,17 +11,15 @@ describe('dgeni:app', function () {
       .inDir(path.join(os.tmpdir(), './temp-test'))
       .withOptions({ 'skip-install': true })
       .withPrompt({
-        someOption: true
+        moduleName: 'testModule',
+        ownerName: 'testOwner'
       })
       .on('end', done);
   });
 
   it('creates files', function () {
     assert.file([
-      'bower.json',
-      'package.json',
-      '.editorconfig',
-      '.jshintrc'
+      'bower.json'
     ]);
   });
 });
