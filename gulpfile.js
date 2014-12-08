cb3c15ebe69ce129af05c80072b0f7fcf087d69f
Yosuke Kurami <yosuke.kurami@gmail.com>
2014-12-08 20:40:35 +0900

    Add gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var $ = require('gulp-load-plugins')({
+  pattern: ['gulp-*', 'main-bower-files', 'uglify-save-license', 'del']
+});
+
+gulp.task('jshint', function () {
+  return gulp.src('app/**/*.js')
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.size());
+});
+
+gulp.task('mocha', function () {
+  return gulp.src('test/**/*.js')
+    .pipe($.mocha({
+      reporter: 'spec'
+    }))
+    .pipe($.size());
+});
+
+gulp.task('test', ['mocha', 'jshint']);
+
+gulp.task('default', ['test']);
+
